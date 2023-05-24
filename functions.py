d31810d508b063394f1307efc4e32b4122c9a449
Marley <mrl.menard@gmail.com>
2023-05-24 12:31:32 +0200

    help

--- functions.py
+++ functions.py
@@ -1,5 +1,6 @@
 import random
 
+
 def load_quotes(filename):
     quotes = []
     with open(filename, 'r') as file:
@@ -10,12 +11,15 @@ def load_quotes(filename):
             quotes.append(line)
     return quotes
 
+
 def random_quote(quotes):
     random_quote = random.choice(quotes)
     return random_quote
 
+
 def print_quote(quote):
     print(quote)
+
 
 def view_quotes(quotes):
     for quote in quotes:
